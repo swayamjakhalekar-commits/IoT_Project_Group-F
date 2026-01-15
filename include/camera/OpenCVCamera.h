df96c41b635b7d4e930e6f7871db06ccec08cf3c
swayamjakhalekar-commits <swayamjakhalekar@gmail.com>
2026-01-15 21:23:21 +0100

    Modify capture method to accept SharedState
    Updated capture method to include SharedState parameter.

--- include/camera/OpenCVCamera.h
+++ include/camera/OpenCVCamera.h
@@ -1,10 +1,16 @@
 #pragma once
+
 #include "camera/CameraInterface.h"
+#include "system/SharedState.h"
+#include <opencv2/opencv.hpp>
 
 class OpenCVCamera : public CameraInterface {
 public:
     OpenCVCamera();
-    bool capture(cv::Mat& frame) override;
+
+    // Capture frame and update SharedState timing
+    bool capture(cv::Mat& frame, SharedState& shared_state) override;
+
 private:
     cv::VideoCapture cap_;
 };
