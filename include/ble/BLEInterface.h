a8977e146f46a6e6883c21883bcf582fd25c7a7d
swayamjakhalekar-commits <swayamjakhalekar@gmail.com>
2026-01-15 21:45:39 +0100

    Modify sendData to include SharedState parameter
    Updated BLEInterface to include shared state in sendData method.

--- include/ble/BLEInterface.h
+++ include/ble/BLEInterface.h
@@ -2,12 +2,22 @@
 #define BLE_INTERFACE_H
 
 #include <string>
+#include "system/SharedState.h"
 
 class BLEInterface {
 public:
     virtual ~BLEInterface() = default;
-    virtual void sendData(const std::string& data);
-    virtual std::string receiveData();
+
+    /**
+     * @brief Send data over BLE and timestamp transmission
+     *
+     * @param data         Encoded control command
+     * @param shared_state Shared system state for timing
+     */
+    virtual void sendData(const std::string& data,
+                          SharedState& shared_state) = 0;
+
+    virtual std::string receiveData() = 0;
 };
 
 #endif
