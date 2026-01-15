6da9dba4ae7764f778f9175980a778701461b116
swayamjakhalekar-commits <swayamjakhalekar@gmail.com>
2026-01-15 21:13:22 +0100

    Add TimeUtils.h for time utility functions

---
+++ include/system/TimeUtils.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <chrono>
+#include <cstdint>
+
+inline uint64_t now_ns() {
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(
+        std::chrono::steady_clock::now().time_since_epoch()
+    ).count();
+}
