6124f96ca84f848f9b2252861cf2b20dcf723df3
swayamjakhalekar-commits <swayamjakhalekar@gmail.com>
2026-02-16 22:01:18 +0100

    Refactor now_ns to nowNs and add namespace

--- include/system/TimeUtils.h
+++ include/system/TimeUtils.h
@@ -3,8 +3,12 @@
 #include <chrono>
 #include <cstdint>
 
-inline uint64_t now_ns() {
+namespace TimeUtils {
+
+inline uint64_t nowNs() {
     return std::chrono::duration_cast<std::chrono::nanoseconds>(
         std::chrono::steady_clock::now().time_since_epoch()
     ).count();
+}
+
 }
