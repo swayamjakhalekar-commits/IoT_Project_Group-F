30a008702ca6a5b8d99ff8dabf6388701cfc8a83
swayamjakhalekar-commits <swayamjakhalekar@gmail.com>
2026-02-16 20:24:50 +0100

    Refactor capture method in OpenCVCamera
    Removed SharedState parameter from capture method.

--- include/camera/OpenCVCamera.h
+++ include/camera/OpenCVCamera.h
@@ -1,15 +1,13 @@
 #pragma once
 
 #include "camera/CameraInterface.h"
-#include "system/SharedState.h"
 #include <opencv2/opencv.hpp>
 
 class OpenCVCamera : public CameraInterface {
 public:
     OpenCVCamera();
 
-    // Capture frame and update SharedState timing
-    bool capture(cv::Mat& frame, SharedState& shared_state) override;
+    bool capture(cv::Mat& frame) override;
 
 private:
     cv::VideoCapture cap_;
