4c2f786892e9dce47e3f5321e7e9872b05207cf6
swayamjakhalekar-commits <swayamjakhalekar@gmail.com>
2026-02-16 20:24:14 +0100

    Refactor capture method in CameraInterface

--- include/camera/CameraInterface.h
+++ include/camera/CameraInterface.h
@@ -1,13 +1,11 @@
 #pragma once
 
 #include <opencv2/opencv.hpp>
-#include "system/SharedState.h"
 
 class CameraInterface {
 public:
     virtual ~CameraInterface() = default;
 
-    // Capture frame and update shared timing state
-    virtual bool capture(cv::Mat& frame, SharedState& shared_state) = 0;
+    // Capture frame only (timing handled in main thread)
+    virtual bool capture(cv::Mat& frame) = 0;
 };
-
