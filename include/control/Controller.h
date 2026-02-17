832bdfbf57d4c350cf647ec4c87eee33925b1c91
swayamjakhalekar-commits <swayamjakhalekar@gmail.com>
2026-02-17 11:57:59 +0100

    Refactor compute method to use SharedState::ControlCommand
    Updated compute method to return SharedState::ControlCommand instead of ControlCommand.

--- include/control/Controller.h
+++ include/control/Controller.h
@@ -2,18 +2,12 @@
 
 #include "system/SharedState.h"
 
-struct ControlCommand {
-    double steering;
-    double speed;
-};
-
 class Controller {
 public:
     /**
      * @brief Compute control command from perception state
-     *
      * @param shared_state Shared system state (errors + timestamps)
-     * @return ControlCommand steering + speed
+     * @return SharedState::ControlCommand steering + speed
      */
-    ControlCommand compute(SharedState& shared_state);
+    SharedState::ControlCommand compute(SharedState& shared_state);
 };
