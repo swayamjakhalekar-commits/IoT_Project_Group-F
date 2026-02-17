5bbfad61d54b51d879ab5c38100e28200d000ac2
swayamjakhalekar-commits <swayamjakhalekar@gmail.com>
2026-02-17 12:04:10 +0100

    Refine documentation for compute method
    Updated documentation for compute method parameters and return type.

--- include/control/Controller.h
+++ include/control/Controller.h
@@ -6,8 +6,8 @@ class Controller {
 public:
     /**
      * @brief Compute control command from perception state
-     * @param shared_state Shared system state (errors + timestamps)
-     * @return SharedState::ControlCommand steering + speed
+     * @param shared_state Shared system state
+     * @return SharedState::ControlCommand
      */
     SharedState::ControlCommand compute(SharedState& shared_state);
 };
